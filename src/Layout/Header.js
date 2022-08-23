453f620964bc768e38f8eee1229f58072baf1f3b
Emma Pickard <emma.joannemarie21@gmail.com>
2022-08-23 17:32:25 -0400

    adjusted for qualified testing

--- src/Layout/Header.js
+++ src/Layout/Header.js
@@ -4,7 +4,7 @@ function Header() {
   return (
     <header className="jumbotron bg-warning">
       <div className="container text-white">
-        <h1 className="display-4 flip">The Flip</h1>
+        <h1 className="display-4 flip">The Flash-O-Matic</h1>
         <h2 className="lead flip">A Flashcard App</h2>
       </div>
     </header>
